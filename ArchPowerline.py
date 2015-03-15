7dcafbe53df1a81fb72666253ecf54a3a8806de7
SlyDeath <belyaev.rs@live.ru>
2015-03-15 22:53:37 +0300

    Update ArchPowerline.py
    Python 3.4 update

--- ArchPowerline.py
+++ ArchPowerline.py
@@ -11,13 +11,12 @@ install = input('Y/n: ')
 
 if install != 'n':
     os.system('/usr/bin/sudo pacman -S python-pip git wget')
-    os.system('pip install git+git://github.com/Lokaltog/powerline')
-    os.system('wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf')
-    os.system('wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf')
-    os.system('mv PowerlineSymbols.otf /usr/share/fonts/')
-    os.system('fc-cache -vf')
-    os.system('mv 10-powerline-symbols.conf /etc/fonts/conf.d/')
-    os.system('/usr/bin/sudo cp -R /usr/lib/python3.3/site-packages/powerline /usr/lib/python3.4/site-packages')
+    os.system('/usr/bin/sudo pip install git+git://github.com/powerline/powerline')
+    os.system('wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf')
+    os.system('wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf')
+    os.system('/usr/bin/sudo mv PowerlineSymbols.otf /usr/share/fonts/')
+    os.system('/usr/bin/sudo fc-cache -vf')
+    os.system('/usr/bin/sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/')
 
 print('#####################################')
 print('# Куда добавить поддержу powerline? #')
@@ -45,47 +44,25 @@ fi\n''')
         bashrc.close()
 
 if choice == '3' or choice == '4':
-    print('##########################################')
-    print('# Установка в GVIM или компиляция клона? #')
-    print('#         1 - gvim                       #')
-    print('#         2 - скомпилировать клон        #')
-    print('##########################################')
+    print('###################')
+    print('# Установка в VIM? #')
+    print('#     1 - да      #')
+    print('#     2 - нет     #')
+    print('###################')
     vimchoice = input('1/2: ')
 
     if vimchoice == '1':
-        checkgvim = os.system('pacman -Q gvim')
+        checkgvim = os.system('pacman -Q vim')
         if checkgvim != 0:
-            print('###########################################')
-            print('# Установить GVIM или компилировать клон? #')
-            print('#         1 - установить gvim             #')
-            print('#         2 - скомпилировать клон         #')
-            print('###########################################')
+            print('##################')
+            print('# Установить VIM? #')
+            print('#     1 - да     #')
+            print('#     2 - нет     #')
+            print('##################')
             vimchoice = input('1/2: ')
 
             if vimchoice == '1':
-                os.system('/usr/bin/sudo pacman -S gvim')
-
-    if vimchoice == '2':
-        print('################################################################################')
-        print('# Скомпилировать VIM с поддержкой python 3.4? (необходимо для powerline)       #')
-        print('# Чтобы не было конфликтов с официальным VIM будет создана программа-клон      #')
-        print('# Для использования VIM с поддержкой python3.4 будет назначена команда "vimpy" #')
-        print('################################################################################')
-        vim = input('Y/n: ')
-        if vim != 'n':
-            os.system('wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2')
-            os.system('tar xvjf vim-7.4.tar.bz2')
-            os.system('cd vim74/ && ./configure --enable-python3interp --enable-rubyinterp --enable-gui=no --without-x --enable-cscope --enable-multibyte --prefix=/usr')
-            os.system('cd vim74/ && make')
-            os.system('/usr/bin/sudo mkdir /usr/local/bin/vimpy')
-            os.system('/usr/bin/sudo cp -R vim74/src/* /usr/local/bin/vimpy')
-            os.system('rm vim-7.4.tar.bz2 && rm -R vim74/')
-            with open(home + '/.zshenv', 'a') as zshenv:
-                zshenv.write('\nalias vimpy="/usr/local/bin/vimpy/vim"\n')
-                zshenv.close()
-            with open(home + '/.bashrc', 'a') as bashrc:
-                bashrc.write('\nalias vimpy="/usr/local/bin/vimpy/vim"\n')
-                bashrc.close()
+                os.system('/usr/bin/sudo pacman -S vim')
 
     with open(home + '/.vimrc', 'a') as vimrc:
         vimrc.write('''\nset rtp+=/usr/lib/python3.4/site-packages/powerline/bindings/vim/
