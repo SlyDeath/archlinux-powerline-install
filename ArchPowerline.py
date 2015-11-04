dfbab949564778b00d63323cf82283e337da34f1
SlyDeath <belyaev.rs@live.ru>
2015-11-04 11:29:29 +0300

    Update ArchPowerline.py
    Убран хардкод версии Python. Теперь, после обновления Python, можно запустить скрипт снова, пропустить установку powerline и шрифтов т.о. получится апдейт к новой версии. :)

--- ArchPowerline.py
+++ ArchPowerline.py
@@ -3,11 +3,14 @@
 __author__ = 'belyaev.rs@live.ru'
 
 import os
+import sys
 
 print('##############################################')
 print('# Установить powerline и необходимые шрифты? #')
 print('##############################################')
 install = input('Y/n: ')
+
+python_version = str(sys.version_info[0]) + "." + str(sys.version_info[1])
 
 if install != 'n':
     os.system('/usr/bin/sudo pacman -S python-pip git wget')
@@ -31,41 +34,21 @@ home = os.getenv('HOME')
 
 if choice == '1' or choice == '4':
     with open(home + '/.zshrc', 'a') as zshrc:
-        zshrc.write('''\nif [[ -r /usr/lib/python3.4/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
-    source /usr/lib/python3.4/site-packages/powerline/bindings/zsh/powerline.zsh
+        zshrc.write('''\nif [[ -r /usr/lib/python''' + python_version + '''/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
+    source /usr/lib/python''' + python_version + '''/site-packages/powerline/bindings/zsh/powerline.zsh
 fi\n''')
         zshrc.close()
 
 if choice == '2' or choice == '4':
     with open(home + '/.bashrc', 'a') as bashrc:
-        bashrc.write('''\nif [[ -r /usr/lib/python3.4/site-packages/powerline/bindings/bash/powerline.sh ]]; then
-    source /usr/lib/python3.4/site-packages/powerline/bindings/bash/powerline.sh
+        bashrc.write('''\nif [[ -r /usr/lib/python''' + python_version + '''/site-packages/powerline/bindings/bash/powerline.sh ]]; then
+    source /usr/lib/python''' + python_version + '''/site-packages/powerline/bindings/bash/powerline.sh
 fi\n''')
         bashrc.close()
 
 if choice == '3' or choice == '4':
-    print('###################')
-    print('# Установка в VIM? #')
-    print('#     1 - да      #')
-    print('#     2 - нет     #')
-    print('###################')
-    vimchoice = input('1/2: ')
-
-    if vimchoice == '1':
-        checkgvim = os.system('pacman -Q vim')
-        if checkgvim != 0:
-            print('##################')
-            print('# Установить VIM? #')
-            print('#     1 - да     #')
-            print('#     2 - нет     #')
-            print('##################')
-            vimchoice = input('1/2: ')
-
-            if vimchoice == '1':
-                os.system('/usr/bin/sudo pacman -S vim')
-
     with open(home + '/.vimrc', 'a') as vimrc:
-        vimrc.write('''\nset rtp+=/usr/lib/python3.4/site-packages/powerline/bindings/vim/
+        vimrc.write('''\nset rtp+=/usr/lib/python''' + python_version + '''/site-packages/powerline/bindings/vim/
 set laststatus=2
 " Использовать 256 цветов (Только если ваш терминал поддерживает 256 цветов)
 set t_Co=256\n''')
